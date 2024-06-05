28259e97eaff758a51cbccd2656c20b3e385bdc4
oleg <kosarevoleglena@gmail.com>
2024-06-05 15:46:04 +0300

    [IMP] web_chatter_position: ODOO-1 added try/except in session_info

--- web_chatter_position/models/ir_http.py
+++ web_chatter_position/models/ir_http.py
@@ -1,5 +1,9 @@
+import logging
 from odoo import models
 from odoo.http import request
+
+
+_logger = logging.getLogger(__name__)
 
 
 class IrHttp(models.AbstractModel):
@@ -11,6 +15,10 @@ class IrHttp(models.AbstractModel):
     #----------------------------------------------------------
     
     def session_info(self):
-        result = super(IrHttp, self).session_info()
-        result['chatter_position'] = self.env.user.chatter_position
-        return result
+        try:
+            result = super(IrHttp, self).session_info()
+            result['chatter_position'] = self.env.user.chatter_position
+            return result
+        except ValueError as e:
+            _logger.warning(e)
+            return _('Not session info')
